86244323d3bca710f6333716d6339177a945db97
Ivy <ruizivyrose97@gmail.com>
2017-11-11 14:37:04 +0800

    Test Connection 11/11/17

---
+++ TestConnection/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace TestConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnapply = new System.Windows.Forms.Button();
+            this.btntest = new System.Windows.Forms.Button();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.txtuid = new System.Windows.Forms.TextBox();
+            this.txtdatabase = new System.Windows.Forms.TextBox();
+            this.txtport = new System.Windows.Forms.TextBox();
+            this.txtserver = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnapply
+            // 
+            this.btnapply.BackColor = System.Drawing.Color.White;
+            this.btnapply.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnapply.ForeColor = System.Drawing.Color.Black;
+            this.btnapply.Location = new System.Drawing.Point(258, 234);
+            this.btnapply.Name = "btnapply";
+            this.btnapply.Size = new System.Drawing.Size(87, 31);
+            this.btnapply.TabIndex = 23;
+            this.btnapply.Text = "Apply";
+            this.btnapply.UseVisualStyleBackColor = false;
+            // 
+            // btntest
+            // 
+            this.btntest.BackColor = System.Drawing.Color.White;
+            this.btntest.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntest.ForeColor = System.Drawing.Color.Black;
+            this.btntest.Location = new System.Drawing.Point(135, 234);
+            this.btntest.Name = "btntest";
+            this.btntest.Size = new System.Drawing.Size(117, 31);
+            this.btntest.TabIndex = 22;
+            this.btntest.Text = "Test Connection";
+            this.btntest.UseVisualStyleBackColor = false;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpassword.ForeColor = System.Drawing.Color.Black;
+            this.txtpassword.Location = new System.Drawing.Point(135, 194);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.PasswordChar = '*';
+            this.txtpassword.Size = new System.Drawing.Size(209, 22);
+            this.txtpassword.TabIndex = 21;
+            // 
+            // txtuid
+            // 
+            this.txtuid.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtuid.ForeColor = System.Drawing.Color.Black;
+            this.txtuid.Location = new System.Drawing.Point(135, 155);
+            this.txtuid.Name = "txtuid";
+            this.txtuid.Size = new System.Drawing.Size(209, 22);
+            this.txtuid.TabIndex = 20;
+            // 
+            // txtdatabase
+            // 
+            this.txtdatabase.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtdatabase.ForeColor = System.Drawing.Color.Black;
+            this.txtdatabase.Location = new System.Drawing.Point(135, 114);
+            this.txtdatabase.Name = "txtdatabase";
+            this.txtdatabase.Size = new System.Drawing.Size(209, 22);
+            this.txtdatabase.TabIndex = 19;
+            // 
+            // txtport
+            // 
+            this.txtport.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtport.ForeColor = System.Drawing.Color.Black;
+            this.txtport.Location = new System.Drawing.Point(135, 74);
+            this.txtport.Name = "txtport";
+            this.txtport.Size = new System.Drawing.Size(209, 22);
+            this.txtport.TabIndex = 18;
+            // 
+            // txtserver
+            // 
+            this.txtserver.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtserver.ForeColor = System.Drawing.Color.Black;
+            this.txtserver.Location = new System.Drawing.Point(135, 41);
+            this.txtserver.Name = "txtserver";
+            this.txtserver.Size = new System.Drawing.Size(209, 22);
+            this.txtserver.TabIndex = 17;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.Black;
+            this.label10.Location = new System.Drawing.Point(29, 190);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(100, 26);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Password:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Black;
+            this.label9.Location = new System.Drawing.Point(29, 155);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(56, 26);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "UID:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(29, 114);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(100, 26);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Database:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(29, 74);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 26);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Port:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(29, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 26);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Server:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 313);
+            this.Controls.Add(this.btnapply);
+            this.Controls.Add(this.btntest);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtuid);
+            this.Controls.Add(this.txtdatabase);
+            this.Controls.Add(this.txtport);
+            this.Controls.Add(this.txtserver);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnapply;
+        private System.Windows.Forms.Button btntest;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.TextBox txtuid;
+        private System.Windows.Forms.TextBox txtdatabase;
+        private System.Windows.Forms.TextBox txtport;
+        private System.Windows.Forms.TextBox txtserver;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
